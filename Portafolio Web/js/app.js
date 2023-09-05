e2724928882dc39dfa5f3325afb10f81b0364c39
JHO <107950868+JHOODRELL@users.noreply.github.com>
2023-09-05 11:01:54 -0400

    Add files via upload

---
+++ Portafolio Web/js/app.js
@@ -0,0 +1,15 @@
+document.getElementById("menu").addEventListener("click", mostrar_menu);
+document.getElementById("back-menu").addEventListener("click", ocultar_menu);
+
+nav = document.getElementById("nav");
+background_menu = document.getElementById("back-menu");
+
+function mostrar_menu(){
+    nav.style.right = "0px";
+    background_menu.style.display = "block";
+}
+
+function ocultar_menu(){
+    nav.style.right = "-250px";
+    background_menu.style.display = "none";
+}
